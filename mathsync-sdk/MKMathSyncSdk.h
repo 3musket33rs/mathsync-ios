ed2992291c4dc4719b6c9ad204b62f88aa6af72b
corinnekrych <corinnekrych@gmail.com>
2014-02-27 00:51:35 +0100

    fix typo

--- mathsync-sdk/MKMathSyncSdk.h
+++ mathsync-sdk/MKMathSyncSdk.h
@@ -3,12 +3,13 @@
 #define _MATH_SYNC_
 
 #import "MKSerializer.h"
-#import "MKDeserializer"
+#import "MKDeserializer.h"
 #import "MKSummarizer.h"
 #import "MKSummarizerFromItems.h"
 #import "MKSummarizerFromJson.h"
 #import "MKResolver.h"
-#import "MKResolverFromSummarizer.h"
+#import "MKResolverFromSummarizers.h"
+
 
 #endif /* _MATH_SYNC_ */
 
